2a9470d476bcec0bde5b283f53419c73ffb6c81c
Benchaib-Mohamed <benchaibmohamed06@gmail.com>
2025-03-17 15:06:51 +0100

    script compil, deploiement et exec front ok

--- scriptBack.sh
+++ scriptBack.sh
@@ -8,7 +8,7 @@ PATH_BACK_VM="/home/toto/sirius/backend/ecoptimize-backend-1.0-SNAPSHOT-jar-with
 PATH_BACK="/Users/mohamedbenchaib/ECOPTIMIZE/ecoptimize-backend/target/ecoptimize-backend-1.0-SNAPSHOT-jar-with-dependencies.jar"
 
 
-scp -r 
+scp -r $PATH_BACK $USER@$IP:"/home/toto/sirius/backend"
 # connexion à la vm back et lancement du serveur 
 sshpass -p "$PWD" ssh $USER@$IP "java -jar $PATH_BACK_VM"
 
